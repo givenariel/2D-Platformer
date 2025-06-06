50082d2999c07c5a61c431368f58911644dc311b
givenariel <hokuace1972@gmail.com>
2025-06-06 19:05:42 +0700

    2025-6-6 commit

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -3,33 +3,84 @@ using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
 {
-    private IA_Player playerControls;
-    private Rigidbody2D rb;
+    [SerializeField]
+    private float moveSpeed = 10f,
+    jumpForce = 6f,
+    groundDetectLength = 1.5f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private int jumpCount;
+    [SerializeField] private Transform groundDetect;
+    private Vector2 moveInput;
+    private RaycastHit2D hit;
+    float horizontalInput;
 
-    void Awake()
+    private void Update()
     {
-        playerControls = new IA_Player();
-        rb = GetComponent<Rigidbody2D>();
-    }
+        Grounded();
+        Flip();
 
-    private void OnEnable()
-    {
-        playerControls.Enable();
-        playerControls.Player.Jump.performed += Jump;
-    }
-
-    void OnDisable()
-    {
-        playerControls.Disable();
-        playerControls.Player.Jump.performed -= Jump;
-    }
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (context.performed)
+        if (Grounded())
         {
-            Debug.Log("Jump");
-            rb.AddForceY(50f);
+            jumpCount = 0;
         }
+        // if (!Grounded())
+        // {
+        //     rb.gravityScale = 7;
+        // }
+        // else
+        // {
+        //     rb.gravityScale = 1;
+        // }
+    }
+
+    void FixedUpdate()
+    {
+        horizontalInput = moveInput.x;
+        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocityY);
+    }
+
+    private void OnMovement(InputValue inputValue)
+    {
+        moveInput = inputValue.Get<Vector2>();
+    }
+
+    private void OnJump()
+    {
+        if (jumpCount < 2)
+        {
+            rb.AddForceY(jumpForce);
+            jumpCount++;
+        }
+    }
+
+    private bool Grounded()
+    {
+        hit = Physics2D.Raycast(groundDetect.position, -groundDetect.up, groundDetectLength, groundLayer);
+        if (hit)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void Flip()
+    {
+        if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(groundDetect.position, -groundDetect.up, Color.red);
     }
 }
